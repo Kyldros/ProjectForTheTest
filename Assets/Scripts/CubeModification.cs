347a6d22ac2f257bfa1a49c8673eda3b6a85030e
Kyldros <kyldros@gmail.com>
2023-03-09 15:53:28 +0100

    Modifiche finite

--- Assets/Scripts/CubeModification.cs
+++ Assets/Scripts/CubeModification.cs
@@ -4,11 +4,23 @@ using UnityEngine;
 
 public class CubeModification : MonoBehaviour
 {
-    public float rotationValue;
+    public float rotationValue = 0.5f;
+    public float colorChangeDuration = 1.5f;
+    public Color firstColor = Color.red;
+    public Color secondColor = Color.green;
+    private Renderer rend;
+
+    private void Start()
+    {
+        rend = GetComponent<Renderer>();
+    }
 
     // Update is called once per frame
     void Update()
     {
         transform.Rotate(transform.position, rotationValue);
+
+        float lerp = Mathf.PingPong(Time.time, colorChangeDuration) / colorChangeDuration;
+        rend.material.color = Color.Lerp(firstColor, secondColor, lerp);
     }
 }
